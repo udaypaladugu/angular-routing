ca3254918aa2aad45d17912782d1207363aa9e0f
udaypaladugu <uday.paladugu@gmail.com>
2017-04-14 14:01:17 -0400

    routing components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from "app/dashboard/dashboard.component";
+import { CustomerComponent } from "app/customer/customer.component";
 
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', component: DashboardComponent},
+  { path: 'customer', component: CustomerComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
